96f49bc99d986f9c48850d97283958764b2be66a
Arvind Satyanarayan <arvindsatya@google.com>
2017-10-18 16:33:03 -0700

    Use swoopy arrow to explain activation vector vis.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ExamplePicker from './diagrams/ExamplePicker.html';
 import VectorExplain from './diagrams/VectorExplain.html';
-import ActivationVis from './diagrams/ActivationVis.html';
+import ActivationVecExplain from './diagrams/ActivationVecExplain.html';
 
 const exPick = new ExamplePicker({
   target: document.getElementById('ExamplePicker')
@@ -10,13 +10,14 @@ const vecExpl = new VectorExplain({
   target: document.getElementById('VectorExplain')
 });
 
-const actVis = new ActivationVis({
-  target: document.getElementById('ActivationVis')
+const actVis = new ActivationVecExplain({
+  target: document.getElementById('ActivationVecExplain')
 });
 
 // Wire components together.
-exPick.observe('selected', (selected) => {
-  vecExpl.set({example: selected});
+exPick.observe('selected', (example) => {
+  vecExpl.set({example});
+  actVis.set({example});
 });
 
 exPick.observe('act', (act) => {
