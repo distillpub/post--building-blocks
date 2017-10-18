7779a250eb3f67f2f134b7e0fed15b9c2acda7d9
Arvind Satyanarayan <arvindsatya@google.com>
2017-10-18 13:52:59 -0700

    Iterating on activation vis diagrams.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ExamplePicker from './diagrams/ExamplePicker.html';
 import VectorExplain from './diagrams/VectorExplain.html';
+import ActivationVis from './diagrams/ActivationVis.html';
 
 const exPick = new ExamplePicker({
   target: document.getElementById('ExamplePicker')
@@ -7,7 +8,11 @@ const exPick = new ExamplePicker({
 
 const vecExpl = new VectorExplain({
   target: document.getElementById('VectorExplain')
-})
+});
+
+const actVis = new ActivationVis({
+  target: document.getElementById('ActivationVis')
+});
 
 // Wire components together.
 exPick.observe('selected', (selected) => {
@@ -19,5 +24,14 @@ exPick.observe('act', (act) => {
 });
 
 exPick.observe('max_act', (max_act) => {
-  vecExpl.set({max_act})
+  vecExpl.set({max_act});
+  actVis.set({max_act});
+});
+
+vecExpl.observe('pos', (pos) => {
+  actVis.set({pos});
+});
+
+vecExpl.observe('present_vector', (present_vector) => {
+  actVis.set({present_vector});
 });
